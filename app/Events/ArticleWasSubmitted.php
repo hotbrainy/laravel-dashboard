7790a692166141ba0d37c15ad3d7cb60c5aca433
Web Dev <super.crazyboy429@yandex.com>
2017-02-23 04:32:40 -0800

    feat: article submit create

---
+++ app/Events/ArticleWasSubmitted.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class ArticleWasSubmitted
+{
+    use InteractsWithSockets, SerializesModels;
+
+    public $user;
+    public $article;
+    public $is_Admin;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($user , $article,$is_Admin)
+    {
+        //
+        $this->user = $user;
+        $this->article = $article;
+        $this->is_Admin = $is_Admin;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}

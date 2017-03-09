723c40999e69553347175cefa005446e87755d10
Magento Developer <super.crazyboy429@yandex.com>
2017-03-09 08:44:17 -0800

    initial dashboard pages

---
+++ public/js/controller.js
@@ -0,0 +1,18 @@
+(function () {
+
+
+    // Article Functions will goes here.
+    /**
+     * bind information Review Model
+     * Like paragraph ,words and characters
+     */
+
+
+
+
+
+    /*$('#myModal').on('shown.bs.modal', function () {
+        $('#myInput').focus()
+    })*/
+
+});
